177e7422a5893f7a13bd3026ae3aba4352197cd5
deadboy <andrey.swift@gmail.com>
2023-10-25 20:45:07 +0300

    Добавил расширение для отладки и для прозрачного фона

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -33,9 +33,61 @@ M.general = {
       end,
       "Open lazy",
     },
+    ["tg"] = {
+      function()
+        require("transparent").toggle()
+      end,
+      "Toggle transparent background",
+    },
   },
 }
 
--- more keybinds!
+-- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt
+M.debug = {
+  n = {
+    ["<F5>"] = {
+      function()
+        require("dap").continue()
+      end,
+      "continue",
+    },
+    ["<F10>"] = {
+      function()
+        require("dap").step_over()
+      end,
+      "step over",
+    },
+    ["<F11>"] = {
+      function()
+        require("dap").step_into()
+      end,
+      "step into",
+    },
+    ["<F12>"] = {
+      function()
+        require("dap").step_out()
+      end,
+      "step out",
+    },
+    ["<leader>b"] = {
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      "Toggle breakpoint",
+    },
+    ["<leader>B"] = {
+      function()
+        require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ")
+      end,
+      "Add breakpoint with condition",
+    },
+    ["<leader>dl"] = {
+      function()
+        require("dap").run_last()
+      end,
+      "Restart debugger",
+    },
+  },
+}
 
 return M
